226deb9840c2afbf87b56b9af80558850ade1adb
Balázs Máté <balazsm86@gmail.com>
2017-09-29 02:36:16 +0200

    Creating tests for Destination class

--- src/test/java/com/emarsys/holidayplanner/DestinationTest.java
+++ src/test/java/com/emarsys/holidayplanner/DestinationTest.java
@@ -1,7 +1,19 @@
 package com.emarsys.holidayplanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DestinationTest {
+    @Test
+    void testNewDestinationName() {
+        Destination destination = new Destination("x");
+        assertEquals("x", destination.destinationName);
+    }
 
+    @Test
+    void testPreviousDestinationOfNewDestination() {
+        Destination destination = new Destination("x");
+        assertEquals(null, destination.previousDestination);
+    }
 }
